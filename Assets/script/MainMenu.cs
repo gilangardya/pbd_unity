4564d5936278313a616d88828b122c711461b8da
gilangardya <13515096@std.stei.itb.ac.id>
2018-03-21 18:53:09 +0700

    add Main Menu

---
+++ Assets/script/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayGame() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		// pindah scene
+	}
+
+	public void Settings() {
+		
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
