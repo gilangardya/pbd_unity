45bfee69e62b22f2784340e1527f4ee29172b832
gilangardya <13515096@std.stei.itb.ac.id>
2018-03-21 19:39:54 +0700

    add transition to scene1

--- Assets/script/MainMenu.cs
+++ Assets/script/MainMenu.cs
@@ -7,8 +7,8 @@ public class MainMenu : MonoBehaviour {
 
 	// Use this for initialization
 	public void PlayGame() {
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 		// pindah scene
+		Application.LoadLevel("scene1");
 	}
 
 	public void Settings() {
