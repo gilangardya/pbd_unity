c504b3291f826ef2bd78e83068ef31905b8c77e6
stevannohero <stevannohero@gmail.com>
2018-03-21 23:01:22 +0700

    add sound effect

---
+++ Assets/script/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayGame() {
+		// pindah scene
+		Application.LoadLevel("scene1");
+	}
+
+	public void Settings() {
+		
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
