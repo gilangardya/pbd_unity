1dcf4e8aceda2ebef8d862ee5178a2939e9712f5
gilangardya <13515096@std.stei.itb.ac.id>
2018-03-21 21:37:08 +0700

    add mixer

---
+++ Assets/script/SettingMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingMenu : MonoBehaviour {
+
+	public AudioMixer audioMixer;
+
+	// Use to change value
+	public void SetVolume (float volume) {
+		audioMixer.SetFloat ("volume", volume);
+	}
+}
