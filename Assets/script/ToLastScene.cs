7412830d7ab3718a2164823f52658e861e55564c
stevannohero <stevannohero@gmail.com>
2018-03-22 17:04:45 +0700

    add transition scene scripts

---
+++ Assets/script/ToLastScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToLastScene : MonoBehaviour {
+	void OnTriggerEnter(Collider col) {
+		if (col.CompareTag("Player")) {
+			SceneManager.LoadScene("scene2");
+		}
+	}
+}
